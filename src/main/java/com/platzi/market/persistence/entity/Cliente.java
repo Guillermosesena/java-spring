b4761703c8a404c46c937989ebce6c68678c1bb6
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-10 14:50:01 -0500

    16/41 Composite key

--- src/main/java/com/platzi/market/persistence/entity/Cliente.java
+++ src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -2,12 +2,14 @@ package com.platzi.market.persistence.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "clientes")
 public class Cliente {
 
+    @Id
     private Integer id;
     private String nombre;
 
